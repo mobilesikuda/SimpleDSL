17a4c0dce14fc3920412ffe2f40bfd59cfe7945d
Сергей Кудашкин <ya.sikuda@yandex.ru>
2023-01-09 00:28:04 +0300

    step1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.time.LocalDateTime.now
+
+val kotlinxHtmlVersion = project.extra.properties["kotlinxHtmlVersion"].toString()
 
 plugins {
     kotlin("jvm") version "1.7.21"
@@ -6,18 +9,15 @@ plugins {
 }
 
 group = "ru.sikuda"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
-    //maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
     mavenCentral()
 }
 
 dependencies {
 
-    implementation ("com.google.code.gson:gson:2.10")
-
-    val kotlinxHtmlVersion = "0.7.3"
+    implementation("com.google.code.gson:gson:2.10")
 
     // include for JVM target
     implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinxHtmlVersion")
@@ -44,4 +44,64 @@ tasks.withType<KotlinCompile> {
 application {
     mainClass.set("MainKt")
 }
+
+tasks.register("Hello world") {
+    doLast {
+        println("Hello world")
+    }
+}
+
+tasks.register("IncreaseVersion") {
+    println("Old version=$version")
+    version = "1.0.1"
+    println("New version=$version")
+}
+
+tasks.register("meta-log") {
+
+    shouldRunAfter("build")
+
+    println("Group: ${project.group}")
+    println("Version: ${version}")
+    val artifactId = project.name
+    println("artifactId: ${artifactId}")
+    println("Time run: ${now()}")
+
+}.get()
+
+tasks.named("build") {
+    finalizedBy("meta-log")
+}
+
+tasks {
+    this.named("jar")
+    withType<Jar> {
+        manifest {
+            attributes("Main-Class" to "MainKt")
+        }
+    }
+
+    task<Jar>("fatJar") {
+        archiveBaseName.set(project.name + "-fat-jar")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+        manifest {
+            attributes("Main-Class" to "MainKt")
+        }
+        from(
+            configurations.runtimeClasspath.get()
+                .map { if (it.isDirectory) it else zipTree(it) }
+        )
+        with(named("jar").get() as CopySpec)
+    }
+
+    build {
+        dependsOn("fatJar")
+    }
+}
+
+
+// Include dependent libraries in archive.
+val mainClassName = "com.company.application.Main"
+
 
